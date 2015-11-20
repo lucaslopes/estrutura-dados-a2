1aad3f2970ff78172a89cd28376876b885b2392d
Lucas Lopes <lucaslopesf2@gmail.com>
2015-11-19 23:44:20 -0200

    Update code.c

--- code.c
+++ code.c
@@ -4,17 +4,17 @@
 #include <conio.h>
 #include <string.h>
 
-//Estrutura agenda
+//Estrutura do estoque
 typedef struct estoque {
     char nome[20];
     int codigo;
 } estoque;
 
-//Varivaies globais
+//Variaveis globais
 FILE *arq;
 estoque peca;
 
-//Funcao menu, onde retorna alguma opcao
+//Retornar alguma opcao do menu
 char menu() {
     system("cls");
     printf("\n");
@@ -30,7 +30,7 @@ char menu() {
     return (toupper(getche()));
 }
 
-//Funcao para verificar se no arquivo "banco.bin" já existe uma peca, retornando 1
+//Verificar se já existe uma peca, retornando 1
     int verifica (char nome[]) {
     while (fread(&peca, sizeof(estoque), 1, arq))
     if (!strcmp(peca.nome, nome))
@@ -38,14 +38,11 @@ char menu() {
     return 0;
     }
 
-//Funcao para verificar se no arquivo "banco.bin" já existe o codigo de uma peca, retornando 1
-int verifica_cod(int num)
-{
+//Verificar se já existe o codigo de uma peca, retornando 1
+int verifica_cod(int num) {
     fread(&peca,sizeof(estoque),1,arq);
-    while(!feof(arq))
-    {
-        if (peca.codigo == num)
-        {
+    while(!feof(arq))     {
+        if (peca.codigo == num)         {
             fseek(arq,-sizeof(estoque),SEEK_CUR);
             return 1;
         }
@@ -54,27 +51,27 @@ int verifica_cod(int num)
     return 0;
 }
 
-//Funcao para adicionar uma nova peca ao estoque
+//Adicionar uma nova peca ao estoque
 void adicionar_peca() {
 	
 }
 
-//Funcao para modificar uma peca do estoque
+//Modificar uma peca do estoque
 void alterar_peca() {
 	
 }
 
-//Funcao para remover uma peca do estoque
+//Remover uma peca do estoque
 void remover_peca() {
 	
 }
 
-//Funcao para procurar uma peca no estoque
+//Procurar uma peca no estoque
 void procura_peca() {
 	
 }
 
-//Funcao para mostrar o estoque
+//Mostrar o estoque
 void consultar_estoque() {
 	
 }
